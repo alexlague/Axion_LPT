fb1fd7bb64e390b38856f1d694d0e05673050b60
Alex Laguë <lague@astro.utoronto.ca>
2020-01-08 11:42:50 -0500

    Adding parameter file

---
+++ params.py
@@ -0,0 +1,32 @@
+# Cosmological parameters
+
+H0       = 70 # km/s/Mpc
+Omega_M0 = 0.3
+Omega_L  = 0.7
+
+# Simulations parameters
+
+boxsize  = 256 # Mpc
+grid     = 512
+
+# Axion parameters
+
+ax_mass  = 1e-6 # in units of 10^{-22} eV
+ax_frac  = 0.1  # between 0 and 1
+
+# Wavenumber parameters
+
+kmin     = 1e-2 # in Mpc^{-1}
+kmax     = 1e2  # in Mpc^{-1}
+
+# Redshift of interest
+
+redshift = 1.
+
+# Input file
+
+input_f  = '256Mpc_n512_nb40_nt1_1_28_10'
+
+# Output file name
+
+output_f = '256Mpc_n512_nb40_nt1_1_28_10_fdm_smoothed'
